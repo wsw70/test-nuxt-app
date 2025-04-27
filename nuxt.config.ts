0f129c23403b61b82faf4cca8bd8f22515d3c296
wsw70 <1345886+wsw70@users.noreply.github.com>
2025-04-27 17:00:38 +0200

    added workaround for process issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  preset: "node", // important for Node-style apps
+  externals: {
+    external: [
+      ".prisma", // ignore Prisma internals
+      "@prisma/client", // don't bundle Prisma
+      "process", // don't bundle "process"
+    ],
+    inline: [],
+  },
 });
